6fef1128d0223589b050457b229d55d4df2e6b1f
department-of-observation <113044066+department-of-observation@users.noreply.github.com>
2023-04-27 11:31:51 +0800

    third release code now allows the user to enter several numeric values and before calculating the average, minimum and maximum values for the list of temperature values.

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,44 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+finalinput = 0
+min = 0
+max = 0
+
+
+def calcaverage():
+    average = sum(finalinput) / len(finalinput)
+    print(average)
+
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    get_user_input()
+    calcaverage()
+    find_min_max()
+    sort_temperature()
+    calc_median_temperature()
+
+
+def display_main_menu():
+    print('“Enter some numbers separated by commas (e.g. 5, 67, 32)')
+
+
+def get_user_input():
+    input = input()
+    split = input.split()
+    finalinput = [float(x) for x in split]
+    print('finalinput')
+    return finalinput
+
+
+def find_min_max():
+    min = min(finalinput)
+    max = max(finalinput)
+    return (min, max)
+
+
+def sort_temperature():
+    print('sort_temperature')
+
+
+def calc_median_temperature():
+    print('calc_median_temperature')
